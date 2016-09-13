64c7a0f13c70cff0006f15020def1a35b1d0ea61
Nguyen Duc Tho <lex4vn@gmail.com>
2016-09-13 11:20:24 +0700

    article

--- application/controllers/articles.php
+++ application/controllers/articles.php
@@ -23,5 +23,17 @@ class Articles extends MY_Controller {
 		$this->data['content'] 			= 'articles/news';
 		$this->_render_page('temp/template', $this->data);
 	}
-
+	public function news($id)
+	{
+		$language = $this->session->userdata('site_lang');
+		$language = $language == 'english'? 1 : 2;
+		$table 							= $this->db->dbprefix('articles');
+		$news 				= $this->base_model->run_query(
+			"select id, articles_description.title as title,image,articles_description.body as body,slug,cat_id,pubdate,created  from ".$table." LEFT JOIN articles_description ON articles.id = articles_description.article_id where id=".$id." and lang = ".$language
+		);
+		$this->data['news'] 	= $news;
+		$this->data['active_menu'] 		= 'news';
+		$this->data['content'] 			= 'articles/details';
+		$this->_render_page('temp/template', $this->data);
+	}
  }
